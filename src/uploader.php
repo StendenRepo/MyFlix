7fca563bbd1c1185d9bfeb40655a0cc5a52ecd3b
JesseOost <jesse.oost@student.nhlstenden.com>
2021-12-07 19:51:44 +0100

    created video validator & temporary file safer

---
+++ src/uploader.php
@@ -0,0 +1,59 @@
+<?php
+
+if(isset($_POST["submit_video"]) && isset($_FILES["video"])){
+
+    echo "<pre>";
+    print_r($_FILES["video"]);
+    echo "</pre>";
+
+    $title = $_POST["title"];
+
+    $videoName = $_FILES["video"]["name"];
+    $videoType = $_FILES["video"]["type"];
+    $videoTmpName = $_FILES["video"]["tmp_name"];
+    $videoError = $_FILES["video"]["error"];
+
+    $genres = $_POST["genre"];
+
+    if(validateUpload($title, $videoName, $videoType, $videoTmpName, $videoError, $genres)){
+
+        // Temporary until database is avaiable
+
+        $videoSafeName = uniqid();
+
+        $videoNameSplit = explode('.', $videoName);
+        $videoExtention = strtolower(end($videoNameSplit));
+
+        $videoTmpDestination = __DIR__ . "/../public/tmpUpload/$videoSafeName.$videoExtention";
+        move_uploaded_file($videoTmpName, $videoTmpDestination);
+    }
+
+} else{
+    header("Location: ../public/uploadVideo.php");
+}
+
+function validateUpload($title, $name, $type, $tmpName, $error, $genres){
+    if($error === 0){
+        $allowedExtentions = array("video/mp4", "video/webm", "video/avi", "video/flv");
+        if(in_array($type, $allowedExtentions)){
+            if(!empty($title)){
+                if(!empty($genres)){
+                    return true;
+                } else{
+                    $noGenre = "Make sure to select 1 or more genres.";
+                    header("Location: ../public/uploadVideo.php?error=$noGenre");
+                }
+            } else{
+                $noTitle = "Make sure to give your video a title.";
+                header("Location: ../public/uploadVideo.php?error=$noTitle");
+            }
+        } else{
+            $incType = "Make sure that you upload a .mp4, .webm, .avi or .flv file.";
+            header("Location: ../public/uploadVideo.php?error=$incType");
+        }
+    } else{
+        $fileError = "There was an issue with uploading your file, try again.";
+        header("Location: ../public/uploadVideo.php?error=$fileError");
+    }
+
+?>

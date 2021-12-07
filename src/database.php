c20af2c9c015e6177f75353a11dfb154a24e3f21
ronaldNederveen <ronald.nederveen@student.nhlstenden.com>
2021-12-07 23:13:06 +0100

    added phpdoc to functions for database connection

--- src/database.php
+++ src/database.php
@@ -1,14 +1,26 @@
 <?php
+/**
+ * Create the connection to the database
+ *
+ * @param $install
+ * @return false|mysqli|null
+ */
 function dbConnect($install = false)
 {
-	if ($install) {
-		return mysqli_connect(DB_HOST, DB_USER, DB_PASS);
-	}else{
-		return mysqli_connect(DB_HOST, DB_USER, DB_PASS,DB_DATABASE);
-	}
+    if ($install) {
+        return mysqli_connect(DB_HOST, DB_USER, DB_PASS);
+    } else {
+        return mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
+    }
 }
 
+/**
+ * close the connection to the database
+ *
+ * @param $connection object you made by creating the connection to the database
+ * @return void
+ */
 function dbClose($connection)
 {
-	mysqli_close($connection);
+    mysqli_close($connection);
 }

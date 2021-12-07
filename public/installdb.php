48345991a4a4d2af775959b414feb4d3b327f3d1
ronaldNederveen <ronald.nederveen@student.nhlstenden.com>
2021-12-07 23:16:20 +0100

    removed verbose echo for the installation

--- public/installdb.php
+++ public/installdb.php
@@ -1,43 +1,30 @@
 <?php
 require_once "../src/config.php";
-// TODO add check if the database is installed. if not create the database with all tables.
 
-//
 $connection = dbConnect(true);
-//test connection and existence of database
+
 if (mysqli_connect_errno()) {
-	die("Failed to connect to MySQL: " . mysqli_connect_error());
-}
-try {
-	mysqli_select_db($connection,"myflix");
-}catch(mysqli_sql_exception $exception){
-	echo "could not connect to database";
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 
-echo "database does not exist \nwill now install database.";
-
-$createDatabase = mysqli_prepare($connection, "CREATE DATABASE IF NOT EXISTS `myflix` DEFAULT CHARACTER SET UTF8mb4 COLLATE utf8mb4_general_ci");
-
-mysqli_stmt_execute($createDatabase);
-echo "created database\n";
-
-mysqli_select_db($connection, "myflix");
-echo "using myflix";
-
-$sqlPath = "../src/database/db.sql";
-
-$sql = file_get_contents($sqlPath);
-$sqlArray = explode(";", $sql);
-
-foreach ($sqlArray as $query) {
-	if ($query != "") {
-		$stmnt = mysqli_prepare($connection, $query . ";");
-		mysqli_stmt_execute($stmnt);
-		echo "<pre>executed statement:\n".$query."</pre><hr>";
-	} else {
-		exit;
-	}
+$databasecheck = dbConnect();
+var_dump($databasecheck);
+if (!$databasecheck) {
+    $createDatabase = mysqli_prepare($connection, "CREATE DATABASE IF NOT EXISTS `" . DB_DATABASE . "` DEFAULT CHARACTER SET UTF8mb4 COLLATE utf8mb4_general_ci");
+    mysqli_stmt_execute($createDatabase);
+    mysqli_select_db($connection, "myflix");
+    $sqlPath = "../src/database/db.sql";
+    $sql = file_get_contents($sqlPath);
+    $sqlArray = explode(";", $sql);
+    foreach ($sqlArray as $query) {
+        if ($query != "") {
+            $stmnt = mysqli_prepare($connection, $query . ";");
+            mysqli_stmt_execute($stmnt);
+        } else {
+            break;
+        }
+    }
 }
-
 mysqli_close($connection);
-echo "closed connection";
+header("refresh:2;url= /");
+echo "u wordt terug geleid naar de homepagina";

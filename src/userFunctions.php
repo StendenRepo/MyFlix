db1cb83d000ab14cc35ba30b1352b1a12d744547
Hydra <cedsmit@gmail.com>
2021-12-08 13:46:45 +0100

    changed comment DOC change return type of isValidPassword function
    explicitly typed the type of the arguments
    
    Signed-off-by: Hydra <cedsmit@gmail.com>

--- src/userFunctions.php
+++ src/userFunctions.php
@@ -1,27 +1,31 @@
 <?php
 
 /**
+ * Verifies if the password is valid.
  * The password requirements are
  * at least 1 Uppercase, 1 Lowercase, 1 Number
  * and 8 characters long
  *
- * @param $password
- * @return false|int
+ * @param string $password
+ * @return bool returns true if pattern matches the subject, false if it does not.
  */
-function isValidPassword($password): bool|int
+function isValidPassword(string $password): bool
 {
-    return preg_match("/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$/", $password);
+    if (preg_match("/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$/", $password)) {
+        return true;
+    }
+    return false;
 }
 
 /**
  * Binary comparison
  * matches the password with a confirmed password
  *
- * @param $password
- * @param $confirmPassword
- * @return bool
+ * @param string $password
+ * @param string $confirmPassword
+ * @return bool returns true if password did match with the confirmed password. else returns false
  */
-function didPasswordMatch($password, $confirmPassword): bool
+function didPasswordMatch(string $password, string $confirmPassword): bool
 {
     if (strcmp($password, $confirmPassword) === 0) {
         return true;

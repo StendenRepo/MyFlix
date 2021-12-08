6d9a8bf0e744a2a2171cc27c7c5b3e2d85eb7a8b
ronaldNederveen <ronald.nederveen@student.nhlstenden.com>
2021-12-08 19:51:27 +0100

    rewrote to use the closedb function and the database variable

--- public/installdb.php
+++ public/installdb.php
@@ -10,7 +10,7 @@ if (mysqli_connect_errno() > 0) {
 
 $createDatabase = mysqli_prepare($connection, "CREATE DATABASE IF NOT EXISTS `" . DB_DATABASE . "` DEFAULT CHARACTER SET UTF8mb4 COLLATE utf8mb4_general_ci");
 mysqli_stmt_execute($createDatabase);
-mysqli_select_db($connection, "myflix");
+mysqli_select_db($connection, DB_DATABASE);
 $sqlPath = "../src/database/db.sql";
 $sql = file_get_contents($sqlPath);
 $sqlArray = explode(";", $sql);
@@ -23,7 +23,7 @@ foreach ($sqlArray as $query) {
     }
 }
 
-mysqli_close($connection);
+dbClose($connection);
 header("refresh:2;url= /");
 echo "u wordt terug geleid naar de homepagina";
 

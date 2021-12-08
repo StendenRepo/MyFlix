8dcc96a0ce1ef77e8abb3926fa2eb3ca09f76f91
ronaldNederveen <ronald.nederveen@student.nhlstenden.com>
2021-12-08 19:41:34 +0100

    made installscript for database functional

--- public/installdb.php
+++ public/installdb.php
@@ -3,27 +3,27 @@ require_once "../src/config.php";
 
 $connection = dbConnect(true);
 
-if (mysqli_connect_errno()) {
+if (mysqli_connect_errno() > 0) {
     die("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 
 
-if (!dbExists()) {
-    $createDatabase = mysqli_prepare($connection, "CREATE DATABASE IF NOT EXISTS `" . DB_DATABASE . "` DEFAULT CHARACTER SET UTF8mb4 COLLATE utf8mb4_general_ci");
-    mysqli_stmt_execute($createDatabase);
-    mysqli_select_db($connection, "myflix");
-    $sqlPath = "../src/database/db.sql";
-    $sql = file_get_contents($sqlPath);
-    $sqlArray = explode(";", $sql);
-    foreach ($sqlArray as $query) {
-        if ($query != "") {
-            $stmnt = mysqli_prepare($connection, $query . ";");
-            mysqli_stmt_execute($stmnt);
-        } else {
-            break;
-        }
+$createDatabase = mysqli_prepare($connection, "CREATE DATABASE IF NOT EXISTS `" . DB_DATABASE . "` DEFAULT CHARACTER SET UTF8mb4 COLLATE utf8mb4_general_ci");
+mysqli_stmt_execute($createDatabase);
+mysqli_select_db($connection, "myflix");
+$sqlPath = "../src/database/db.sql";
+$sql = file_get_contents($sqlPath);
+$sqlArray = explode(";", $sql);
+foreach ($sqlArray as $query) {
+    if ($query != "") {
+        $stmnt = mysqli_prepare($connection, $query . ";");
+        mysqli_stmt_execute($stmnt);
+    } else {
+        break;
     }
 }
+
 mysqli_close($connection);
 header("refresh:2;url= /");
 echo "u wordt terug geleid naar de homepagina";
+

95b2ac181b80b3e306aa9e0c3336722a420010e1
RonaldNed <ronald.nederveen@student.nhlstenden.com>
2021-12-09 12:10:06 +0100

    added space for readability of script

--- public/installdb.php
+++ public/installdb.php
@@ -7,10 +7,10 @@ if (mysqli_connect_errno() > 0) {
     die("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 
-
 $createDatabase = mysqli_prepare($connection, "CREATE DATABASE IF NOT EXISTS `" . DB_DATABASE . "` DEFAULT CHARACTER SET UTF8mb4 COLLATE utf8mb4_general_ci");
 mysqli_stmt_execute($createDatabase);
 mysqli_select_db($connection, DB_DATABASE);
+
 $sqlPath = "../src/database/db.sql";
 $sql = file_get_contents($sqlPath);
 $sqlArray = explode(";", $sql);
@@ -26,4 +26,3 @@ foreach ($sqlArray as $query) {
 dbClose($connection);
 header("refresh:2;url= /");
 echo "u wordt terug geleid naar de homepagina";
-

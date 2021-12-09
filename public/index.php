8a2959438c0c7548f3cd68da6501ffbc2fea8c35
giovannidevries <giovanni.de.vries@student.nhlstenden.com>
2021-12-09 15:12:12 +0100

    Klinkbare afbeeldingen

--- public/index.php
+++ public/index.php
@@ -11,19 +11,19 @@ showHeader($extraCss = ['assets/css/home.css']);
     <div class="content">
         <h1>Placeholder</h1>
         <div class="images">
-            <img src="assets/img/Placeholders/Placeholder_1.jpg" alt="placeholder 1">
-            <img src="assets/img/Placeholders/Placeholder_2.jpg" alt="placeholder 2">
-            <img src="assets/img/Placeholders/Placeholder_3.jpg" alt="placeholder 3">
-            <img src="assets/img/Placeholders/Placeholder_4.jpg" alt="placeholder 4">
-            <img src="assets/img/Placeholders/Placeholder_5.jpg" alt="placeholder 5">
+            <a href="index.php"><img src="assets/img/Placeholders/Placeholder_1.jpg" alt="placeholder 1"></a>
+            <a href="index.php"><img src="assets/img/Placeholders/Placeholder_2.jpg" alt="placeholder 2"></a>
+            <a href="index.php"><img src="assets/img/Placeholders/Placeholder_3.jpg" alt="placeholder 3"></a>
+            <a href="index.php"><img src="assets/img/Placeholders/Placeholder_4.jpg" alt="placeholder 4"></a>
+            <a href="index.php"><img src="assets/img/Placeholders/Placeholder_5.jpg" alt="placeholder 5"></a>
         </div>
         <h1>Placeholder</h1>
         <div class="images">
-            <img src="assets/img/Placeholders/Placeholder_6.jpg" alt="placeholder 6">
-            <img src="assets/img/Placeholders/Placeholder_7.jpg" alt="placeholder 7">
-            <img src="assets/img/Placeholders/Placeholder_8.jpg" alt="placeholder 8">
-            <img src="assets/img/Placeholders/Placeholder_9.jpg" alt="placeholder 9">
-            <img src="assets/img/Placeholders/Placeholder_10.jpg" alt="placeholder 10">
+            <a href="index.php"><img src="assets/img/Placeholders/Placeholder_6.jpg" alt="placeholder 6"></a>
+            <a href="index.php"><img src="assets/img/Placeholders/Placeholder_7.jpg" alt="placeholder 7"></a>
+            <a href="index.php"><img src="assets/img/Placeholders/Placeholder_8.jpg" alt="placeholder 8"></a>
+            <a href="index.php"><img src="assets/img/Placeholders/Placeholder_9.jpg" alt="placeholder 9"></a>
+            <a href="index.php"><img src="assets/img/Placeholders/Placeholder_10.jpg" alt="placeholder 10"></a>
         </div>
     </div>
 

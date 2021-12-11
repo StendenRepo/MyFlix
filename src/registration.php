942a39799f46faedbe1371679671a75229174ee7
Cedric Smit <cedsmit@gmail.com>
2021-12-11 18:22:35 +0100

    changes error reporting can now show more complicated errors are once.

--- src/registration.php
+++ src/registration.php
@@ -15,7 +15,9 @@ $inputArray = filter_input_array(INPUT_POST, [
 $errorMsg = [
     "required" => "This is field required",
     "password" => "Password must be at least 8 characters long and contain atleast 1 lower case, 1 upper case and 1 numbers",
-    "matchPassword" => "Passwords do NOT match"
+    "matchPassword" => "Passwords do NOT match",
+    "exceedUsernameLength" => "Username cannot exceed 20 characters",
+    "exceedEmailLength" => "Email cannot exceed 64 characters",
 ];
 
 if (!isset($_POST["submit"])) return;
@@ -28,24 +30,30 @@ if (in_array(null, $inputArray)) {
             $errors[$key] = $errorMsg["required"];
         }
     }
+    // User has not filled in all fields no point in going further.
     return;
 }
 
+
 if (!checkStrLength($inputArray["username"], 20))
-    return $errors["username"] = "Username cannot exceed 20 characters";
+    $errors["username"] = $errorMsg["exceedUsernameLength"];
 
 if (!checkStrLength($inputArray["email"], 64))
-    return $errors["email"] = "Email cannot exceed 64 characters";
+    $errors["email"] = $errorMsg["exceedEmailLength"];
 
 if (!isValidPassword($inputArray["pw"]))
-    return $errors["pw"] = $errorMsg["password"];
+    $errors["pw"] = $errorMsg["password"];
 
 if (!didPasswordMatch($inputArray["pw"], $inputArray["confirm-pw"]))
-    return $errors["confirm-pw"] = $errorMsg["matchPassword"];
+    $errors["confirm-pw"] = $errorMsg["matchPassword"];
+
+if (isset($errors)) {
+    return;
+}
 
 //TODO:  Ask leraar voor een beter oplossing dan dit.
 //tijdelijk return errors van register en print die op de pagina uit.
-
+//After we make sure every fields meets the requirements then we start the register process.
 $errors = register($inputArray["username"], $inputArray["email"], $inputArray["pw"]);
 
 

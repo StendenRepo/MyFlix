3244c3546863676c608a8075e315cdfd0cdf6643
FrancaBaars <91067514+FrancaBaars@users.noreply.github.com>
2021-12-13 11:52:47 +0100

    CSS login pagina en wat SQL

--- public/login.php
+++ public/login.php
@@ -1,52 +1,68 @@
 <?php
 require_once "../src/config.php";
-showHeader($extra_css = ['assets/css/template.css']);
+showHeader(['assets/css/login.css']);
 ?>
 
 <?php
 $email = "";
 $password = "";
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if(!empty($_POST["email"]) && !empty($_POST["password"])) {
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-        $sql = "test@test.com";
-        $sql2 = "test";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (!empty($_POST["email"]) && !empty($_POST["password"])) {
+		$email = $_POST["email"];
+		$password = $_POST["password"];
+		$sql = "test@test.com";
+		$sql2 = "test";
+//		$conn = dbConnect();
 //        $sql = "SELECT emailadress FROM account WHERE emailadress=$email";
 //        $stmtLogin = mysqli_prepare($conn, $sql) OR DIE("stmt errer");
 //
 //        mysqli_stmt_execute($stmtLogin) OR DIE("stmt execute error");
 //
 //        mysqli_stmt_close($stmtLogin);
-        if($email == $sql){
-            if($password == $sql2){
-                echo "you are logged in";
-            }
-            else{
-                echo "wrong information";
-            }
-        }else{
-            echo "wrong information";
-        }
-    }else{
-        echo "please fill in both";
-    }
+//        $sql2= "SELECT password FROM account WHERE emailadres = $email";
+//        if()
+		if ($email == $sql) {
+			if ($password == $sql2) {
+				echo "you are logged in";
+			} else {
+				echo "wrong information";
+			}
+		} else {
+			echo "wrong information";
+		}
+	} else {
+		echo "please fill in both";
+	}
 }
 
 ?>
 
     <body>
-    <h1>Login</h1>
 
-    <form method="post" action="">
-        <label for="email">E-mail</label>
-        <input type="email" id="email" name="email" placeholder="E-mail adress">
-        <label for="password">Password</label>
-        <input  type="password" id="password" name="password" placeholder="Password">
+    <div class="flex-container">
+        <div class="LoginImg">
+            <img src="assets/img/logo.png">
+        </div>
 
-        <input type="submit" value="Log in">
-    </form>
+        <div class="LoginForm">
+            <form method="post" action="login.php">
+
+                <label for="email">E-mail</label>
+                <input type="email" id="email" name="email" placeholder="E-mail adress" class="LoginInput">
+
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Password" class="LoginInput">
+                <div class="test">
+                    <input type="submit" value="Log in" class="LoginButton">
+                </div>
+            </form>
+        </div>
+        <div class="LoginNew">
+            <p>New to MyFlix?</p>
+            <a href=""">Register</a>
+        </div>
+    </div>
 
     </body>
 <?php showFooter(); ?>

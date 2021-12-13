9955c80cbec3adcd0b4bf43ca04f5583e49fb6d3
Youri <3141748-BeatRoy@users.noreply.gitlab.com>
2021-12-13 12:13:52 +0100

    Added template for the header and removed the paddings from the template.css

--- src/templates/header.php
+++ src/templates/header.php
@@ -1,26 +1,15 @@
 <?php
 /**
- * This function shows the header and if needed extra css can be loaded
- * @param array $extraCss
- * @param bool $useTemplate
+ * This function shows the header
+ * @param bool $showLogo
+ * @param bool $showSearch
+ * @param bool $showMyAccount
  */
-function showHeader(array $extraCss = [], bool $useTemplate = true) {
-    // When use_template=true load the template.css else create an empty variable
-    $css = ($useTemplate) ? '<link rel="stylesheet" href="assets/css/template.css">' . "\n" : '';
-    // When there are extra_css files provided add them to the css variable
-    if (sizeof($extraCss) > 0) {
-        foreach ($extraCss as $cssLink) {
-            $css .= '<link rel="stylesheet" href="' . htmlspecialchars($cssLink) . '">' . "\n";
-        }
-    }
-    echo '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyFlix</title>
-    ' . $css . '
-</head>' . "\n";
-}
+function showHeader(bool $showLogo = true, bool $showSearch = true, bool $showMyAccount = true) { ?>
+    <div class="header">
+        <?= ($showLogo) ? '<a href="index.php"><img src="assets/img/logo.png" alt="logo" height="45"></a>' . "\n" : ''; ?>
+        <?= ($showSearch) ? '<input type="text" placeholder="Search">' . "\n" : ''; ?>
+        <?= ($showMyAccount) ? '<a href="#" class="red-btn-filled">My Account</a>' . "\n" : ''; ?>
+    </div>
+<?php }
 

5a2563f82f24368c0f8d09d6e78d79e17a642700
DennisSchomaker <Dennisschomaker852@gmail.com>
2021-12-15 12:24:33 +0100

    created base of page
    created the file, added head & header

---
+++ public/videoMod.php
@@ -0,0 +1,10 @@
+<?php
+require_once "../src/config.php";
+showHead("videoMod", ['assets/css/videoMod.css']);
+?>
+    <head>
+    <?php showHeader(); ?>
+        <div>
+
+        </div>
+    </head>

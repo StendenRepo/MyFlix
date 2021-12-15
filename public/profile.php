cefeb0d2729f76a30ccf53125ec2c63dd7e6a60f
Sylvana <56027003+SylvanaStenden@users.noreply.github.com>
2021-12-15 12:09:15 +0100

    DB Connection
    Made DB connection and attempting to load rows into the form.

--- public/profile.php
+++ public/profile.php
@@ -1,6 +1,6 @@
 <?php
 require_once "../src/config.php";
-showHeader(['assets/css/profile.css']);
+showHead("profile", (['assets/css/profile.css']));
 ?>
 
 <!DOCTYPE html>
@@ -13,82 +13,89 @@ showHeader(['assets/css/profile.css']);
         </head>
 
         <body>
-            <?php
-                include 'profileConfig.php';
-            ?>
+            <div id="profileContainer">
+                <div id = "titleContainer">
+                    <h2 id="profileTitle">Your Profile</h2>
+                </div>
 
-        <div id="profileContainer">
-            <div id = "titleContainer">
-                <h2 id="profileTitle">Your Profile</h2>
+                <div id = "formContainer">
+                    <form action="" method="post">
+                        <?php
+                            $conn = dbConnect() or die("Database connection failed");
+                            $sql = "SELECT * FROM account";
+                            $result = mysqli_query($conn, $sql);
+
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
+
+                        <div class="inputBox">
+                            <label for="userName">Username</label>
+                            <input type="text" id="userName" name="userName" disabled "<?php echo $row['username']; ?>">
+                        </div>
+                        <div class="inputBox">
+                            <label for="eMail">E-mail address</label>
+                            <input type="email" id="eMail" name="eMail" "<?php echo $row['email']; ?>">
+                        </div>
+                        <div class="inputBox">
+                            <label for="passWord">Password</label>
+                            <input type="password" id="passWord" name="passWord" placeholder="Password" "<?php echo $row['password']; ?>">
+                        </div>
+                        <div class="inputBox">
+                            <label for="cPassWord">Confirm password</label>
+                            <input type="password" id="cPassWord" name="cPassWord" placeholder="Re-enter your password" "<?php echo $row['password']; ?>">
+                        </div>
+                        <div class="inputBox">
+                            <label for="firstName">First name</label>
+                            <input type="text" id="firstName" name="firstName" placeholder="First name" value="<?php echo $row['firstName']; ?>">
+                        </div>
+                        <div class="inputBox">
+                            <label for="lastName">Last name</label>
+                            <input type="text" id="lastName" name="lastName" value="<?php echo $row['lastName']; ?>">
+                        </div>
+
+                        <?php
+                                }
+                            }
+                        ?>
+
+                        <div class="inputBox">
+                            <label for="companyName">Company name</label>
+                            <input type="text" id="companyName" name="companyName" placeholder="Company name">
+                        </div>
+                        <div class="inputBox">
+                            <label for="address">Address</label>
+                            <input type="text" id="address" name="address" placeholder="Address">
+                        </div>
+                        <div class="inputBox">
+                            <label for="postalCode">Postal Code</label>
+                            <input type="text" id="postalCode" name="postalCode" placeholder="Postal code">
+                        </div>
+                        <div class="inputBox">
+                            <label for="city">City</label>
+                            <input type="text" id="city" name="city" placeholder="City">
+                        </div>
+                        <div class="inputBox">
+                            <label for="country">Country</label>
+                            <input type="text" id="country" name="country" placeholder="Country">
+                        </div>
+                        <div class="inputBox">
+                            <label for="bankAccount">Bank Account Number</label>
+                            <input type="text" id="bankAccount" name="bankAccount" placeholder="Bank Account Number">
+                        </div>
+                        <div class="inputBox">
+                            <label for="accountType">Account Type</label>
+                            <select id="accountType" name="accountType">
+                                <option value="viewer">Viewer</option>
+                                <option value="contentCreator">Content Creator</option>
+                            </select>
+                        </div>
+                        <div id="button">
+                            <input type="submit" value="Update Profile">
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div id = "formContainer">
-                <form action="" method="post">
-                    <?php
-
-                        $sql = "";
-
-                    ?>
-                    <div class="inputBox">
-                        <label for="userName">Username</label>
-                        <input type="text" id="userName" name="userName" disabled>
-                    </div>
-                    <div class="inputBox">
-                        <label for="eMail">E-mail address</label>
-                        <input type="email" id="eMail" name="eMail" placeholder="E-mail">
-                    </div>
-                    <div class="inputBox">
-                        <label for="passWord">Password</label>
-                        <input type="password" id="passWord" name="passWord" placeholder="Password">
-                    </div>
-                    <div class="inputBox">
-                        <label for="cPassWord">Confirm password</label>
-                        <input type="password" id="cPassWord" name="cPassWord" placeholder="Re-enter your password">
-                    </div>
-                    <div class="inputBox">
-                        <label for="companyName">Company name</label>
-                        <input type="text" id="companyName" name="companyName" placeholder="Company name">
-                    </div>
-                    <div class="inputBox">
-                        <label for="firstName">First name</label>
-                        <input type="text" id="firstName" name="firstName" placeholder="First name">
-                    </div>
-                    <div class="inputBox">
-                        <label for="lastName">Last name</label>
-                        <input type="text" id="lastName" name="lastName" placeholder="Last name">
-                    </div>
-                    <div class="inputBox">
-                        <label for="address">Address</label>
-                        <input type="text" id="address" name="address" placeholder="Address">
-                    </div>
-                    <div class="inputBox">
-                        <label for="postalCode">Postal Code</label>
-                        <input type="text" id="postalCode" name="postalCode" placeholder="Postal code">
-                    </div>
-                    <div class="inputBox">
-                        <label for="city">City</label>
-                        <input type="text" id="city" name="city" placeholder="City">
-                    </div>
-                    <div class="inputBox">
-                        <label for="country">Country</label>
-                        <input type="text" id="country" name="country" placeholder="Country">
-                    </div>
-                    <div class="inputBox">
-                        <label for="bankAccount">Bank Account Number</label>
-                        <input type="text" id="bankAccount" name="bankAccount" placeholder="Bank Account Number">
-                    </div>
-                    <div class="inputBox">
-                        <label for="accountType">Account Type</label>
-                        <select id="accountType" name="accountType">
-                            <option value="viewer">Viewer</option>
-                            <option value="contentCreator">Content Creator</option>
-                        </select>
-                    </div>
-                    <div id="button">
-                        <input type="submit" value="Update Profile">
-                    </div>
-                </form>
-            </div>
-        </div>
 
         </body>
     </html>

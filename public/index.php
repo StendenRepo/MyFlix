3fe9c345da7f5a8ff57f7af6277b4a9144ab3ce2
Youri <3141748-BeatRoy@users.noreply.gitlab.com>
2021-12-22 09:16:05 +0100

    Added space to comments

--- public/index.php
+++ public/index.php
@@ -4,49 +4,49 @@ showHead("Videos", ['assets/css/home.css']);
 ?>
 
 <?php
-//connection database
+// Make a connection with the database
 $conn = dbConnect();
 
-//query
+// Define the SQL Query
 $sql = "SELECT *, genre.name AS genreName 
         FROM genre JOIN film ON genre.id = film.genreId 
         WHERE film.accepted = 1
         ORDER BY genreName, film.name";
 
-//get results
+// Get results from the database
 $result = mysqli_query($conn, $sql);
 $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-//make array
+// Creates an empty array
 $genres = [];
 
-//give nested array a name
+// Give nested array a name
 foreach ($data as $row) {
-	$genres[$row['genreName']][] = $row;
+    $genres[$row['genreName']][] = $row;
 }
 
 ?>
     <body>
-	<?php showHeader(); ?>
-    <div class="content">
-		<?php
-		//loop through array and get the data.
-		foreach ($genres as $genre => $items) {
-			//check if there are enough videos in the genre.
-			$amount = min(5, count($items));
-			echo "<div class='genre'>";
-			echo "<h1>" . $genre . "</h1>";
-			echo "<div class='images'>";
-			//takes 5 videos from the genre
-			for ($id = 0; $id < $amount; $id++) {
-				echo "<a href='watch.php?v=" . $items[$id]['id'] . "'>
+        <?php showHeader(); ?>
+        <div class="content">
+            <?php
+            // Loop through array and get the data.
+            foreach ($genres as $genre => $items) {
+                // Check if there are enough videos in the genre.
+                $amount = min(5, count($items));
+                echo "<div class='genre'>";
+                echo "<h1>" . $genre . "</h1>";
+                echo "<div class='images'>";
+                // Takes 5 videos from the genre
+                for ($id = 0; $id < $amount; $id++) {
+                    echo "<a href='watch.php?v=" . $items[$id]['id'] . "'>
 					<img src='" . $items[$id]['thumbnail'] . "' alt='" . $items[$id]['name'] . "'></a>";
-			}
-			echo "</div>";
-			echo "</div>";
-		}
-		?>
-    </div>
+                }
+                echo "</div>";
+                echo "</div>";
+            }
+            ?>
+        </div>
     </body>
 <?php
 showFooter();

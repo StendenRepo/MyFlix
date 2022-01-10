5ac412bef20045e7fb6800730d4b4cfe5d676d91
Youri <3141748-BeatRoy@users.noreply.gitlab.com>
2022-01-10 20:35:08 +0100

    Made the passwordReset.php responsive

--- public/passwordReset.php
+++ public/passwordReset.php
@@ -3,22 +3,16 @@ require_once "../src/config.php";
 
 global $lang;
 
-showHead($lang["resetPassword"], ['assets/css/pwreset.css']);
+showHead($lang["resetPassword"], ['assets/css/auth.css']);
 ?>
     <body>
-        <div id="container">
-            <div id="resetImg">
-                <a href="index.php">
-                    <img src="assets/img/logo.png" alt="Logo MyFlix">
-                </a>
-            </div>
-            <div id="resetMsg">
-                <p id="msg"><?= $lang["resetPw"] ?></p>
-                <h1><a href="mailto:administrationr@myflix.com">administration@myflix.com</a></h1>
-            </div>
-            
-            <a href="login.php"><?= $lang["returnTxt"] ?></a>
-
+        <a href="index.php">
+            <img src="assets/img/logo.png" alt="Logo MyFlix">
+        </a>
+        <div class="resetMsg">
+            <p><?= $lang["resetPw"] ?></p>
+            <a class="link" href="mailto:administration@myflix.com">administration@myflix.com</a>
         </div>
+        <a href="login.php" class="red-btn-filled"><?= $lang["returnTxt"] ?></a>
     </body>
 <?php showFooter(); ?>

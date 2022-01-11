7a781b40b7c3809bd00cfb2da9c3b7e1ccf6fde5
Sylvana <56027003+SylvanaStenden@users.noreply.github.com>
2022-01-11 15:53:58 +0100

    Update profileUpdate.php

--- public/profileUpdate.php
+++ public/profileUpdate.php
@@ -3,33 +3,33 @@
 require_once "../src/config.php";
 
 if (isset($_POST['update'])) {
-	$conn = dbConnect();
+    $conn = dbConnect();
 
-	$accountId = $_SESSION["userId"];
+    $accountId = $_SESSION["userId"];
 
-	$eMail = filter_var($_POST['eMail'], FILTER_SANITIZE_EMAIL);
-	$studioName = filter_var($_POST['studioName'], FILTER_SANITIZE_STRING);
-	$address = filter_var($_POST['address'], FILTER_SANITIZE_STRING);
-	$city = filter_var($_POST['city'], FILTER_SANITIZE_STRING);
-	$bankAccount = filter_var($_POST['bankAccount'], FILTER_SANITIZE_STRING);
+    $eMail = filter_var($_POST['eMail'], FILTER_SANITIZE_EMAIL);
+    $studioName = filter_var($_POST['studioName'], FILTER_SANITIZE_STRING);
+    $address = filter_var($_POST['address'], FILTER_SANITIZE_STRING);
+    $city = filter_var($_POST['city'], FILTER_SANITIZE_STRING);
+    $bankAccount = filter_var($_POST['bankAccount'], FILTER_SANITIZE_STRING);
 
-	$updateSuccess = "Your profile has been updated.";
 	$error = "";
+    $success = "";
 
-	require_once "../src/auth.php";
+    require_once "../src/auth.php";
 
-	if (empty($_POST['eMail']) || empty($_POST['studioName']) || empty($_POST['address']) || empty($_POST['city']) ||
-		empty($_POST['bankAccount'])) {
-		//$error = "One or more fields are empty";
-		echo "One or more fields are empty.";
-	} else {
+    if (empty($_POST['eMail']) || empty($_POST['studioName']) || empty($_POST['address']) || empty($_POST['city']) ||
+        empty($_POST['bankAccount'])) {
+        $error = "One or more fields are empty";
+    } else {
 
-		if (checkEmail($eMail, $accountId) && checkIban($bankAccount, $accountId)) {
-			$sqlCompany = "UPDATE company as c 
+        if (checkEmail($eMail, $accountId) && checkIban($bankAccount, $accountId)) {
+            $sqlCompany = "UPDATE company as c 
                    JOIN account as a on a.companyId = c.id 
                    SET a.email=?, c.studioName=?, c.address=?, c.city=?, c.iban=? 
                    WHERE a.id =?";
 
+<<<<<<< Updated upstream
 			if ($stmtCompany = mysqli_prepare($conn, $sqlCompany)) {
 				if (mysqli_stmt_bind_param($stmtCompany, "sssssi", $eMail, $studioName, $address, $city,
 					$bankAccount, $accountId)) {
@@ -48,4 +48,23 @@ if (isset($_POST['update'])) {
 		}
 	}
 	dbClose($conn);
+=======
+
+            if ($stmtCompany = mysqli_prepare($conn, $sqlCompany)) {
+                if (mysqli_stmt_bind_param($stmtCompany, "sssssi", $eMail, $studioName, $address, $city,
+                    $bankAccount, $accountId)) {
+                    if (!mysqli_stmt_execute($stmtCompany)) {
+                        $error = "Query error in company table <br>";
+                        die(mysqli_error($conn));
+                    }
+                }
+            } else {
+                $error = "Prepare error in company table <br>";
+                die(mysqli_error($conn));
+            }
+            $success = "Your profile has been updated";
+        }
+    }
+    dbClose($conn);
+>>>>>>> Stashed changes
 }

b86cbff158bbee107a93181179b0cb80051c5322
Youri <3141748-BeatRoy@users.noreply.gitlab.com>
2022-01-12 13:05:41 +0100

    Added menu to profile page

--- src/menubar.php
+++ src/menubar.php
@@ -1,21 +1,21 @@
 <?php
-    global $lang;
-    $userLvl = getUserAccountLevel(getCurrentUserId());
+global $lang;
+$userLvl = getUserAccountLevel(getCurrentUserId());
 ?>
-    <div class="menubar">
-        <ul>
-                <H1><?=$lang["manage"]?></H1>
-            <?php if ($userLvl===0){?>
-                <li><a href="index.php"><?=$lang["upgrade"]?></a></li>
-            <?php } elseif ($userLvl===1){?>
-                <li><a href="index.php"><?=$lang["myvideos"]?></a></li>
-                <li><a href="index.php"><?=$lang["uploadvideos"]?></a></li>
-            <?php } elseif ($userLvl===2){?>
-                <li><a href="index.php"><?=$lang["users"]?></a></li>
-                <li><a href="index.php"><?=$lang["allvideos"]?></a></li>
-                <li><a href="index.php"><?=$lang["allstudios"]?></a></li>
-                <?php } ?>
-                <li><a href="index.php"><?=$lang["changepassword"]?></a></li>
-                <li><a href="logout.php"><?=$lang["logout"]?></a></li>
-        </ul>
-    </div>
+<div class="menubar">
+    <ul>
+        <h1><?= $lang["manage"] ?></h1>
+        <?php if ($userLvl === 0) { ?>
+            <li><a href="index.php"><?= $lang["upgrade"] ?></a></li>
+        <?php } elseif ($userLvl === 1) { ?>
+            <li><a href="index.php"><?= $lang["myvideos"] ?></a></li>
+            <li><a href="index.php"><?= $lang["uploadvideos"] ?></a></li>
+        <?php } elseif ($userLvl === 2) { ?>
+            <li><a href="index.php"><?= $lang["users"] ?></a></li>
+            <li><a href="index.php"><?= $lang["allvideos"] ?></a></li>
+            <li><a href="index.php"><?= $lang["allstudios"] ?></a></li>
+        <?php } ?>
+        <li><a href="changePassword.php"><?= $lang["changepassword"] ?></a></li>
+        <li><a href="logout.php"><?= $lang["logout"] ?></a></li>
+    </ul>
+</div>
